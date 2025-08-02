7352e21c72b06d3a8ad21faab9f959bccdd926e3
HANSTS13 <hanstomsojan2028@cs.sjcetpalai.ac.in>
2025-08-02 12:04:44 +0530

    Create Constructor3.java

---
+++ Constructor3.java
@@ -0,0 +1,19 @@
+package module1;
+import java.util.*;
+class Marks{
+	double mark1,mark2,mark3;
+	Marks(double m1,double m2,double m3){
+		mark1=m1;
+		mark2=m2;
+		mark3=m3;
+	}
+	
+}
+public class MarksDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+	}
+
+}
